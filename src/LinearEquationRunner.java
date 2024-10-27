76ded7cec6f1a72b52b2184316e55287a5cf4c60
imran <imranz2@nycstudents.net>
2024-10-26 20:01:31 -0400

    8:01pm 10/26

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,6 +2,7 @@ public class LinearEquationRunner {
     public static void main(String[] args) {
         LinearEquationLogic l1 = new LinearEquationLogic();
         l1.start();
-        System.out.println("");
+        System.out.println("im hooooome!!!!");
+        System.out.println("and its a friday");
     }
 }
